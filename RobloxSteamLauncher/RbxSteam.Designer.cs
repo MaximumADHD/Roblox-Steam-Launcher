fc432e57469ced5465760801941ca10f18073538
Max <MPG1019@aol.com>
2014-10-04 18:29:54 -0500

    Initial Upload

---
+++ RobloxSteamLauncher/RbxSteam.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace RobloxSteamLauncher
+{
+    partial class RbxSteam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RbxSteam));
+            this.LaunchButton = new System.Windows.Forms.Button();
+            this.placeInput = new System.Windows.Forms.TextBox();
+            this.PlaceIDTitle = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.directoryInput = new System.Windows.Forms.TextBox();
+            this.DirectoryTitle = new System.Windows.Forms.Label();
+            this.searchFor = new System.Windows.Forms.Button();
+            this.searchDialog = new System.Windows.Forms.OpenFileDialog();
+            this.credit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LaunchButton
+            // 
+            this.LaunchButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.LaunchButton.Location = new System.Drawing.Point(149, 212);
+            this.LaunchButton.Name = "LaunchButton";
+            this.LaunchButton.Size = new System.Drawing.Size(167, 44);
+            this.LaunchButton.TabIndex = 0;
+            this.LaunchButton.Text = "Launch!";
+            this.LaunchButton.UseVisualStyleBackColor = true;
+            this.LaunchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // placeInput
+            // 
+            this.placeInput.Location = new System.Drawing.Point(12, 225);
+            this.placeInput.Name = "placeInput";
+            this.placeInput.Size = new System.Drawing.Size(114, 20);
+            this.placeInput.TabIndex = 1;
+            // 
+            // PlaceIDTitle
+            // 
+            this.PlaceIDTitle.AutoSize = true;
+            this.PlaceIDTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.PlaceIDTitle.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlaceIDTitle.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.PlaceIDTitle.Location = new System.Drawing.Point(8, 198);
+            this.PlaceIDTitle.Name = "PlaceIDTitle";
+            this.PlaceIDTitle.Size = new System.Drawing.Size(106, 24);
+            this.PlaceIDTitle.TabIndex = 2;
+            this.PlaceIDTitle.Text = "PlaceID:";
+            // 
+            // logo
+            // 
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(12, 4);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(304, 141);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 3;
+            this.logo.TabStop = false;
+            // 
+            // directoryInput
+            // 
+            this.directoryInput.Location = new System.Drawing.Point(12, 175);
+            this.directoryInput.Name = "directoryInput";
+            this.directoryInput.ReadOnly = true;
+            this.directoryInput.Size = new System.Drawing.Size(260, 20);
+            this.directoryInput.TabIndex = 4;
+            // 
+            // DirectoryTitle
+            // 
+            this.DirectoryTitle.AutoSize = true;
+            this.DirectoryTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.DirectoryTitle.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectoryTitle.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.DirectoryTitle.Location = new System.Drawing.Point(8, 148);
+            this.DirectoryTitle.Name = "DirectoryTitle";
+            this.DirectoryTitle.Size = new System.Drawing.Size(238, 24);
+            this.DirectoryTitle.TabIndex = 5;
+            this.DirectoryTitle.Text = "Roblox Application:";
+            // 
+            // searchFor
+            // 
+            this.searchFor.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.searchFor.Location = new System.Drawing.Point(278, 175);
+            this.searchFor.Name = "searchFor";
+            this.searchFor.Size = new System.Drawing.Size(38, 20);
+            this.searchFor.TabIndex = 6;
+            this.searchFor.Text = "...";
+            this.searchFor.UseVisualStyleBackColor = true;
+            this.searchFor.Click += new System.EventHandler(this.searchFor_Click);
+            // 
+            // searchDialog
+            // 
+            this.searchDialog.Filter = "RobloxPlayerBeta | *.exe";
+            this.searchDialog.InitialDirectory = "C:\\Users\\Maxwell\\AppData\\Local";
+            this.searchDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.searchDialog_FileOk);
+            // 
+            // credit
+            // 
+            this.credit.AutoSize = true;
+            this.credit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.credit.ForeColor = System.Drawing.SystemColors.Control;
+            this.credit.Location = new System.Drawing.Point(9, 263);
+            this.credit.Name = "credit";
+            this.credit.Size = new System.Drawing.Size(163, 13);
+            this.credit.TabIndex = 7;
+            this.credit.Text = "@ CloneTrooper1019, 2014 (Version 1.0)";
+            // 
+            // RbxSteam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(328, 285);
+            this.Controls.Add(this.credit);
+            this.Controls.Add(this.searchFor);
+            this.Controls.Add(this.DirectoryTitle);
+            this.Controls.Add(this.directoryInput);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.PlaceIDTitle);
+            this.Controls.Add(this.placeInput);
+            this.Controls.Add(this.LaunchButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RbxSteam";
+            this.Text = "Roblox For Steam";
+            this.Load += new System.EventHandler(this.RbxSteam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LaunchButton;
+        private System.Windows.Forms.TextBox placeInput;
+        private System.Windows.Forms.Label PlaceIDTitle;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.TextBox directoryInput;
+        private System.Windows.Forms.Label DirectoryTitle;
+        private System.Windows.Forms.Button searchFor;
+        private System.Windows.Forms.OpenFileDialog searchDialog;
+        private System.Windows.Forms.Label credit;
+    }
+}
+
---
+++ RobloxSteamLauncher/RbxSteam.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+
+namespace RobloxSteamLauncher
+{
+    public partial class RbxSteam : Form
+    {
+        public RbxSteam()
+        {
+            InitializeComponent();
+        }
+
+        WebClient http = new System.Net.WebClient();
+
+        private void RbxSteam_Load(object sender, EventArgs e)
+        {
+            // Lets see if we can find RobloxPlayerBeta.exe before-hand to save the user time.
+            // If we can't, we'll at least move as close as we can to it.
+
+            string myDir = Environment.GetEnvironmentVariable("LocalAppData");
+            string version = http.DownloadString("http://setup.roblox.com/version");
+            int count = 0;
+            if (version != null)
+            {
+                List<string> paths = new List<string>(
+                    new string[]  { "Roblox", "Versions", version, "RobloxPlayerBeta.exe" }
+                );
+                foreach (string path in paths)
+                {   string nextDir = Path.Combine(myDir, path);
+                    bool isExe = path.Contains("exe");
+                    if (isExe && File.Exists(nextDir) || Directory.Exists(nextDir))
+                    {
+                        myDir = nextDir;
+                        Console.WriteLine("Found: " + nextDir);
+                        count = count + 1;
+                    }
+                }
+            }
+            if (count == 4)
+            {
+                this.directoryInput.Text = myDir;
+            }
+            this.searchDialog.InitialDirectory = myDir;
+        }
+        
+        private void ShowError(string errorMsg)
+        {
+            MessageBox.Show(errorMsg, "An error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            if (this.directoryInput.Text != ""){
+                string text = this.placeInput.Text;
+                int placeId = 0;
+                bool isValid = int.TryParse(text, out placeId);
+                if (isValid == true)
+                {
+                    // Check with roblox to see if this is an actual launchable game.
+                    string requestGame = http.DownloadString("http://www.roblox.com/Game/PlaceLauncher.ashx?request=RequestGame&placeId=" + text);
+                    if (requestGame.Contains("JoinPlace=") || requestGame.Contains("Game/Join.ashx")) // If its joinable, it should contain one of these.
+                    {
+                        this.Hide();
+                        Process Roblox = Process.Start(this.directoryInput.Text, " --id " + text);
+                        Roblox.WaitForExit();
+                        this.Show();
+                    }
+                    else
+                    {
+                        ShowError("Unable to join game! Request was ignored by Roblox. \n(Are you sure this is an actual PlaceId?)");
+                    }
+                }
+                else
+                {
+                    ShowError("Invalid PlaceId Input!");
+                    this.placeInput.Text = "";
+                }
+            }
+            else
+            {
+                ShowError("Roblox's client exe location hasn't been set!");
+            }
+        }
+        bool searchingFor = false;
+        private void searchFor_Click(object sender, EventArgs e)
+        {
+            if (searchingFor == false)
+            {
+                searchingFor = true;
+                this.searchDialog.ShowDialog();
+                searchingFor = false;
+            }
+        }
+
+        private void searchDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            string directory = this.searchDialog.FileName;
+            if (directory.Contains("Roblox"))
+            {
+                if (directory.Contains("RobloxPlayerBeta"))
+                {
+                    this.directoryInput.Text = directory;
+                }
+                else
+                {
+                    ShowError("The exe should be named 'RobloxPlayerBeta' (Try again!)");
+                }
+            }
+            else
+            {
+                ShowError("This directory contains no reference to Roblox. Assuming its invalid. (Try again!)");
+            };
+        }
+    }
+}
